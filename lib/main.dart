e45ef36fb695c1bc8d69b5b8e8a883a4090a47f8
Oscar Henry Torres Caballero <oscar.torres@lighthousetech.io>
2023-05-09 21:10:28 -0400

    GlobalRefreshMixin solution

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -30,12 +32,6 @@ class _MyHomePageState extends State<MyHomePage>
   late int rebuild;
 
   @override
-  void onResume() {
-    _onResumeCallback();
-    super.onResume();
-  }
-
-  @override
   void initState() {
     _counter = 0;
     rebuild = 0;
@@ -48,13 +44,16 @@ class _MyHomePageState extends State<MyHomePage>
     });
   }
 
-  void _onResumeCallback() {
+  @override
+  void onResume() {
+    /// Here you reset the counter to 0 and increment the rebuild counter
     if (mounted) {
       setState(() {
         _counter = 0;
         rebuild++;
       });
     }
+    super.onResume();
   }
 
   @override
@@ -62,19 +61,23 @@ class _MyHomePageState extends State<MyHomePage>
     return Scaffold(
       appBar: AppBar(title: const Text('Lifecycle App')),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            if (rebuild > 0) ...[
-              Text('rebuild amounts: $rebuild'),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'When app is resumed,the widget will rebuild, the counter will be reset to 0 and the rebuild counter will be incremented ($rebuild)',
+                style: Theme.of(context).textTheme.headlineSmall,
+              ),
               const SizedBox(height: 12),
+              const Text('You have pushed the button this many times:'),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
             ],
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -88,16 +91,26 @@ class _MyHomePageState extends State<MyHomePage>
 
 mixin GlobalRefreshMixin<T extends StatefulWidget> on State<T>
     implements WidgetsBindingObserver {
+  /// This method will be called when the app is resumed
+  ///
+  /// You can override this method to do something when the app is resumed
   void onResume() {}
 
   @override
   void initState() {
     super.initState();
+
+    /// Add this line to your initState method
+    ///
+    /// This will register the observer to the widget binding
     WidgetsBinding.instance.addObserver(this);
   }
 
   @override
   void dispose() {
+    /// Add this line to your dispose method
+    ///
+    /// This will unregister the observer from the widget binding
     WidgetsBinding.instance.removeObserver(this);
     super.dispose();
   }
@@ -105,55 +118,64 @@ mixin GlobalRefreshMixin<T extends StatefulWidget> on State<T>
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) {
     if (state == AppLifecycleState.resumed) {
-      // This is where you can trigger a global refresh of your app
+      /// This is where you can trigger a global refresh of your app
       onResume();
     }
   }
 
   @override
   void didChangeAccessibilityFeatures() {
+    log('$runtimeType didChangeAccessibilityFeatures');
     // TODO: implement didChangeAccessibilityFeatures
   }
 
   @override
   void didChangeLocales(List<Locale>? locales) {
+    log('$runtimeType didChangeLocales');
     // TODO: implement didChangeLocales
   }
 
   @override
   void didChangeMetrics() {
+    log('$runtimeType didChangeMetrics');
     // TODO: implement didChangeMetrics
   }
 
   @override
   void didChangePlatformBrightness() {
+    log('$runtimeType didChangePlatformBrightness');
     // TODO: implement didChangePlatformBrightness
   }
 
   @override
   void didChangeTextScaleFactor() {
+    log('$runtimeType didChangeTextScaleFactor');
     // TODO: implement didChangeTextScaleFactor
   }
 
   @override
   void didHaveMemoryPressure() {
+    log('$runtimeType didHaveMemoryPressure');
     // TODO: implement didHaveMemoryPressure
   }
 
   @override
   Future<bool> didPopRoute() {
+    log('$runtimeType didPopRoute');
     // TODO: implement didPopRoute
     throw UnimplementedError();
   }
 
   @override
   Future<bool> didPushRoute(String route) {
+    log('$runtimeType didPushRoute');
     // TODO: implement didPushRoute
     throw UnimplementedError();
   }
 
   @override
   Future<bool> didPushRouteInformation(RouteInformation routeInformation) {
+    log('$runtimeType didPushRouteInformation');
     // TODO: implement didPushRouteInformation
     throw UnimplementedError();
   }
